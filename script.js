d29abb83f0e25bd31e24b978bebdcb6d31e3ee9e
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-05-13 13:35:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length <= 1 || new Set(arr).size === 1) {
+        return -Infinity;
+    }
+    
+    let max = -Infinity;
+    let secondMax = -Infinity;
+    
+    for (let num of arr) {
+        if (num > max) {
+            secondMax = max;
+            max = num;
+        } else if (num > secondMax && num < max) {
+            secondMax = num;
+        }
+    }
+    
+    return secondMax;
+}
+
